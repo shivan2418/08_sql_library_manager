70b7adafce65d12ec77bad95cef056a541b4fcd3
Emil Elgaard <shivan@shivan.dk>
2020-05-28 21:56:37 -0400

    finish book model

--- models/book.js
+++ models/book.js
@@ -3,8 +3,16 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   class Book extends Sequelize.Model {}
   Book.init({
-    title: {type: Sequelize.STRING, allowNull:false},
-    author: {type: Sequelize.STRING, allowNull:false},
+    title: {
+            type: Sequelize.STRING, 
+            allowNull:false,
+            validate: {notEmpty: {msg:'"Title" is required'}}
+          },
+    author: {
+            type: Sequelize.STRING, 
+            allowNull:false,
+            validate: {notEmpty: {msg:'"Author" is required'}}
+            },
     genre: Sequelize.STRING,
     year : Sequelize.INTEGER
   }, { sequelize });
